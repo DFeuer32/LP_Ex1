3b3b4732029778e869e0866f4deb09ec8f02c6ed
TRebelo <>
2020-03-18 23:00:01 +0000

    First commit

---
+++ src/lp/LP.java
@@ -0,0 +1,17 @@
+
+package lp;
+
+/**
+ *
+ * @author Gaming
+ */
+public class LP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
